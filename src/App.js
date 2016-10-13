60a59d5302f54c6e982bf8efaaafc3d65cb6b2d9
balazs4 <balazs4web@gmail.com>
2016-10-13 22:18:08 +0200

    Fetching data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
 import React, {Component} from 'react';
-
+import {get} from 'axios';
+import {decodeHTML} from 'entities';
+import 'js-array-extensions';
 
 
 class App extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
 
+    }
+  }
+
+  componentDidMount() {
+    get('https://api.reddit.com/r/EarthPorn/')
+      .then((res) => {
+        this.setState({
+          content: res.data.data.children.skip(1).where(d => /flickr/.test(d.data.url) === false).map(d => {
+            const url = decodeHTML(d.data.url);
+            return {
+              url: /http:\/\/imgur.com/.test(url) ? url.replace('http://imgur.com', 'http://i.imgur.com') + '.jpg' : url,
+              id: d.data.id
+            };
+          })
+        })
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
 
 
   render() {
+    const {content} = this.state;
+    console.log(content);
     return (
       <div>
+        {content
+          ? content.map(({url, id}) => <img src={url} key={id} style={{ maxWidth: '100%' }} role='presentation'/>)
+          : <div/>
+        }
         <a href="https://www.reddit.com/r/EarthPorn" target="_blank">Source</a>
       </div>
     )
