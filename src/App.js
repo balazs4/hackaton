00fa86968fd93cec6b8c9fc88dfeecf68b54ebc9
balazs4 <balazs4web@gmail.com>
2016-10-13 20:57:49 +0200

    add node-fetch and hi yarn! ;-)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
-import React from 'react';
-const App = () =>
-  <div>
-    <a href="https://www.reddit.com/r/EarthPorn" target="_blank">Source</a>
-  </div>
+import React, {Component} from 'react';
+
+
+
+class App extends Component {
+
+
+
+
+  render() {
+    return (
+      <div>
+        <a href="https://www.reddit.com/r/EarthPorn" target="_blank">Source</a>
+      </div>
+    )
+  }
+}
+
 export default App;
