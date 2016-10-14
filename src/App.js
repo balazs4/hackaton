24441c294e8cd53e175ec73e58b6b53264e6367d
balazs4 <balazs4web@gmail.com>
2016-10-14 20:07:58 +0200

    init redux

--- src/App.js
+++ src/App.js
@@ -1,50 +1,12 @@
-import React, {Component} from 'react';
-import {get} from 'axios';
-import {decodeHTML} from 'entities';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-class App extends Component {
+const App = ({url}) =>
+  <div style={{ width: '100%' }}>
+    <img style={{ width: '100%' }} src={url} alt="EarthPorn" role='presentation' />
+    <a href="https://www.reddit.com/r/EarthPorn" target="_blank">Source</a>
+  </div>
 
-  constructor(props) {
-    super(props);
-    this.state = {}
-  }
-
-  componentDidMount() {
-    get('https://api.reddit.com/r/EarthPorn/')
-      .then((res) => {
-        this.setState({
-          content: res
-            .data
-            .data
-            .children
-            .slice(1)
-            .filter(d => /flickr/.test(d.data.url) === false)
-            .map(d => {
-              const url = decodeHTML(d.data.url);
-              return {
-                url: /http:\/\/imgur.com/.test(url)
-                  ? url.replace('http://imgur.com', 'http://i.imgur.com') + '.jpg'
-                  : url,
-                id: d.data.id
-              };
-            })
-        })
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  }
-
-
-  render() {
-    const {content = []} = this.state;
-    return (
-      <div style={{ width: '100%' }}>
-        {content.map(({url, id}) => <img src={url} key={id} style={{ maxWidth: '100%' }} role='presentation'/>) }
-        <a href="https://www.reddit.com/r/EarthPorn" target="_blank">Source</a>
-      </div>
-    );
-  }
-}
-
-export default App;
+export default connect(
+  state => ({ url: state.url })
+)(App);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,54 @@
 import React from 'react';
-import {render} from 'react-dom';
+import { render } from 'react-dom';
+import { createStore, compose } from 'redux';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import App from './App';
 
+const reducer = (state, {type, payload}) => (state)
+
+const initstate = {url:'https://i.redd.it/fcqheiijjgrx.jpg'}
+
+const store = createStore(
+  reducer,
+  initstate,
+  composeWithDevTools()
+);
+
 render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
+
+
+/*
+import {get} from 'axios';
+import {decodeHTML} from 'entities';
+
+
+ get('https://api.reddit.com/r/EarthPorn/')
+      .then((res) => {
+        this.setState({
+          content: res
+            .data
+            .data
+            .children
+            .slice(1)
+            .filter(d => /flickr/.test(d.data.url) === false)
+            .map(d => {
+              const url = decodeHTML(d.data.url);
+              return {
+                url: /http:\/\/imgur.com/.test(url)
+                  ? url.replace('http://imgur.com', 'http://i.imgur.com') + '.jpg'
+                  : url,
+                id: d.data.id
+              };
+            })
+        })
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+      */
