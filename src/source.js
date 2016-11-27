77b0aae46875b290d78b1c8d6875932f72273c7a
balazs4 <balazs4web@gmail.com>
2016-11-27 19:01:00 +0100

    here we go

--- src/source.js
+++ src/source.js
@@ -4,9 +4,7 @@ export const img$ = Observable
     .ajax('http://piserver:5555/media')
     .switchMap(x => Observable.from(x.response))
     .filter(x => x['tags'].some(y => y === 'ejjel-nappal'))
-    .zip(
-        Observable.interval(5000).startWith(0)
-    )
+    .zip(Observable.interval(5000).startWith(0))
     .map(x => x[0])
     .map(x => ({ title: x['name'], url: x['icon'] }))
     .do(x => console.log(x))
