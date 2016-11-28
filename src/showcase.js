fe85f18a875d9aa0117b083200f9b08685df7c97
balazs4 <balazs4web@gmail.com>
2016-11-28 19:08:40 +0100

    v2.0

--- src/showcase.js
+++ src/showcase.js
@@ -1,9 +1,14 @@
 import { h } from 'preact';
 
-const Showcase = ({source$: {title = "", url = "#"} = {}}, {}) => (
-    <div>
-        <h3>{title}</h3>
-        <img src={url} alt={title} style={{ width: "50%", height: "50%"  }}/>
+const Image = ({url, title}) =>
+    <img src={url} alt={title} style={{
+        "maxWidth": window.innerWidth,
+        "maxHeight": window.innerHeight
+    }} />
+
+const Showcase = ({source$}, {}) => (
+    <div style={{ backgroundColor: 'black', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <Image {...source$} />
     </div>
 )
 
--- src/source.js
+++ src/source.js
@@ -1,22 +1,7 @@
 import { Observable } from 'rxjs';
 
-/*
 export const img$ = Observable
-    .ajax('http://piserver:5555/media')
-    .switchMap(x => Observable.from(x.response))
-    .filter(x => x['tags'].some(y => y === 'ejjel-nappal'))
-    .zip(Observable.interval(5000).startWith(0))
-    .map(x => x[0])
-    .map(x => ({ title: x['name'], url: x['icon'] }))
-    .do(x => console.log(x))
-*/
-
-export const img$ = Observable
-    .ajax({
-        url: `https://api.reddit.com/r/EarthPorn.json`,
-        crossDomain: true,
-        //createXHR: () => new XMLHttpRequest()
-    })
+    .ajax({ url: `https://api.reddit.com/r/EarthPorn.json`, crossDomain: true })
     .map(x => x.response)
     .map(x => x.data.children)
     .switchMap(items => Observable.zip(
