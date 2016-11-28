4144a71d895fc497f4bf9c0c919919a14ade8d87
balazs4 <balazs4web@gmail.com>
2016-11-28 18:32:55 +0100

    it works better now

--- src/source.js
+++ src/source.js
@@ -1,5 +1,6 @@
 import { Observable } from 'rxjs';
 
+/*
 export const img$ = Observable
     .ajax('http://piserver:5555/media')
     .switchMap(x => Observable.from(x.response))
@@ -8,3 +9,20 @@ export const img$ = Observable
     .map(x => x[0])
     .map(x => ({ title: x['name'], url: x['icon'] }))
     .do(x => console.log(x))
+*/
+
+export const img$ = Observable
+    .ajax(`/EarthPorn.json`)
+    .map(x => x.response)
+    .map(x => x.data.children)
+    .switchMap(items => Observable.zip(
+        Observable.timer(0,5000),
+        Observable.from(items)
+    ))
+    .map(x => x[1])
+    .do(x => console.log(x))
+    .map(x => x.data)
+    .map(({ title, thumbnail }) => ({
+        title,
+        url: thumbnail
+    }))
